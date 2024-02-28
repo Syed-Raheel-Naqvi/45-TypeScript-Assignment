fec538a45a874ade7361e83a71ee9486bab46fcc
Syed-Raheel-Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-02-29 01:06:00 +0500

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "Cristiano Ronaldo";
+console.log("lowercase:", personName.toLowerCase());
+console.log("upercase:", personName.toUpperCase());
+console.log("titlecase:", personName.charAt(0).toUpperCase() + personName.slice(1));
---
+++ Q3.ts
@@ -0,0 +1,5 @@
+let personName: string = "Cristiano Ronaldo";
+
+console.log("lowercase:", personName.toLowerCase());
+console.log("upercase:", personName.toUpperCase());
+ console.log("titlecase:", personName.charAt(0).toUpperCase() + personName.slice(1));
