97cf8a98324b03357f2d9f017409e59ee0ba80df
Syed-Raheel-Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-02-29 01:04:33 +0500

    Delete Q3.js

--- Q3.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let personName = "Cristiano Ronaldo";
-console.log("lowercase:", personName.toLowerCase());
-console.log("upercase:", personName.toUpperCase());
-console.log("titlecase:", personName.replace(/\b\w/g, c => c.toUpperCase()));
