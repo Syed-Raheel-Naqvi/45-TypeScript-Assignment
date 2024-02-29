d229eed8a02cc53e124b8eaf20f065922c929297
Syed-Raheel-Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-01 01:58:25 +0500

    Add files via upload

---
+++ Q13.js
@@ -0,0 +1,3 @@
+"use strict";
+let vehicles = ["Rolls Royce Ghost", "Lamborghini Huracan", "Nissan Gtr", "BMW M5"];
+vehicles.map((items) => console.log(`I would like to own a ${items}`));
---
+++ Q13.ts
@@ -0,0 +1,4 @@
+let vehicles: string[] = ["Rolls Royce Ghost", "Lamborghini Huracan", "Nissan Gtr", "BMW M5"]
+
+ vehicles.map((items) => console.log(`I would like to own a ${items}`))
+
