5f1d61a0e2a46b9fce2850f46ef0f2b447e37961
Syed-Raheel-Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-01 01:23:28 +0500

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ["Cristiano", "Hamilton", "Novak", "Dicaprio"];
+let message = "How are you dear?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
