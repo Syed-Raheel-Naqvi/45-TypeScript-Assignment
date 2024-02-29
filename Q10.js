4309bfa5854a511de5e587d2eb3cdad234f08433
Syed-Raheel-Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-01 00:39:28 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,13 @@
+"use strict";
+// Program written by : Syed Raheel Naqvi.
+// Current date: 2-18-2024.
+//This program store famous person's name and message in a variable.
+let famous_person = "Albert Einstein";
+let message = "A person who never made a mistake never tried anything new.";
+console.log(`${famous_person} once said, "${message}"`);
+// Program written by : Syed Raheel Naqvi.
+// Current date: 2-18-2024.
+// This program store a person’s name, and include some whitespace characters at the   beginning and end of the name.
+let personName = "\n\t Cristiano Ronaldo \n\t";
+console.log(personName);
+console.log(personName.trim());
---
+++ Q10.ts
@@ -0,0 +1,23 @@
+
+// Program written by : Syed Raheel Naqvi.
+// Current date: 2-18-2024.
+
+//This program store famous person's name and message in a variable.
+
+let famous_person: string = "Albert Einstein";
+
+let message : string = "A person who never made a mistake never tried anything new."
+
+console.log(`${famous_person} once said, "${message}"`)
+
+
+// Program written by : Syed Raheel Naqvi.
+// Current date: 2-18-2024.
+
+// This program store a person’s name, and include some whitespace characters at the   beginning and end of the name.
+
+let personName: string = "\n\t Cristiano Ronaldo \n\t";
+
+ console.log(personName);
+
+ console.log(personName.trim());
