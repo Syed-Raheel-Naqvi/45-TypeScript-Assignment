98b0f4f8a6de149027e5a1e78e1e91e80fd0561b
Syed-Raheel-Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-02 00:34:15 +0500

    Add files via upload

---
+++ Q17.ts
@@ -0,0 +1,15 @@
+let guest: string[] = ['Kristen', 'Cristiano', 'Novak', 'Ozil', 'Hamilton', 'Elon', "Dicaprio"];
+
+ console.log("Sorry we can't arrange big table, only two people can be invited for dinner.")
+
+ while (guest.length > 2) {
+    let removeGuest = guest.pop();
+    console.log(`Sorry, ${removeGuest} you're no longer invited for dinner.`)
+ }
+guest.forEach((guest) => {
+    console.log(`Dear ${guest}, you're still invited to the dinner.`)
+})
+guest.splice(0,2);
+console.log(guest);
+
+
