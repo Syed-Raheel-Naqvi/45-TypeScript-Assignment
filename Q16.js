bc64df9e5c7d35764878744f517fd845970892e4
Syed-Raheel-Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-01 23:31:15 +0500

    Add files via upload

---
+++ Q16.js
@@ -0,0 +1,12 @@
+"use strict";
+let guestArr = ["Cristiano", "Kristen", "Hamilton", "Dicaprio"];
+let beginning_guest = "Ozil";
+guestArr.unshift(beginning_guest);
+console.log(guestArr);
+let middle_guest = "Novak";
+guestArr.splice(2, 0, middle_guest);
+console.log(guestArr);
+let end_guest = "Elon";
+guestArr.push(end_guest);
+console.log(guestArr);
+guestArr.map((items) => console.log(`Dear ${items}, you are cordinally invited to a dinner.`));
