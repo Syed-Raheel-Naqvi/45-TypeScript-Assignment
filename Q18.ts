84d41bb3c15c20c88342bff4e26f4bd36161fa16
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-08 02:32:21 +0500

    Add files via upload

---
+++ Q18.ts
@@ -0,0 +1,19 @@
+let placesTovisit: string[] = ["Pakistan", "America", "Italy", "France", "Norway"];
+
+console.log("Orginal Order", placesTovisit);
+
+console.log("Alphabetical Order", [...placesTovisit].sort());
+
+console.log("Orginal Order", placesTovisit);
+
+console.log("Reverse Alphabetical Order", [...placesTovisit].sort().reverse());
+
+console.log("Orginal Order", placesTovisit);
+
+console.log("Reversed Order", placesTovisit.reverse());
+
+console.log("Back to its Original Order", placesTovisit.reverse())
+
+console.log("Sorted in Aplhabetical Order", placesTovisit.sort())
+
+console.log("Sorted in Reverse Aplhabetical Order", placesTovisit.sort().reverse())
