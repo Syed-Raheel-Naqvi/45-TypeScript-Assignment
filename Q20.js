e6fc9e6ac1dfb1b99377d21d07d28815ddd91f15
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-09 00:36:00 +0500

    Add files via upload

---
+++ Q20.js
@@ -0,0 +1,3 @@
+"use strict";
+let Cities = ["Cairo", "NewYork", "Paris", "Toronto", "Venice", "Zurich"];
+console.log("Cities I'd like to visit:", Cities);
---
+++ Q20.ts
@@ -0,0 +1,8 @@
+ let Cities: string[] = ["Cairo", "NewYork", "Paris", "Toronto", "Venice", "Zurich"]
+
+ console.log("Cities I'd like to visit:",Cities)
+
+
+
+
+
