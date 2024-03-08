99a234b3b7eb6698d21ebeb117a2a6231b95e158
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-09 04:35:01 +0500

    Add files via upload

---
+++ Q23.ts
@@ -0,0 +1,26 @@
+let car = "subaru"
+
+console.log("is car == 'subaru'?  I predict True.");
+console.log(car == 'subaru'); // True
+console.log("car == 'mercedes'? I predict False.");
+console.log(car == 'mercedes'); //False
+console.log("car.length == 6 ? I predict True.");
+console.log(car.length == 6); //True
+console.log("car.length == 8 ? I predict False.");
+console.log(car.length == 8); //False
+console.log("car.length == 6 && car == 'subaru' I predict True.");
+console.log(car.length == 6 && car == 'subaru'); //True
+console.log("car.length == 8 && car == 'subaru' I predict True.");
+console.log(car.length == 8 && car == 'subaru'); //False
+console.log("Is 25 < 45 ? I predict True.");
+console.log(25 < 45); //True
+console.log("Is 25 > 45 ? I predict False.");
+console.log(25 > 45); //False
+console.log("Is 25 <= 45 ? I predict True.");
+console.log(25 <= 45); //True
+console.log("Is 25 >= 45 ? I predict False.");
+console.log(25 >= 45); //False
+
+
+
+
