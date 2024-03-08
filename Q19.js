6dd4756c1979d12c5d2db1f314399e4e2c599ea9
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-09 00:35:03 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,2 @@
+var guests = ["Cristiano", "Kristen", "Dicaprio", "Tom Cruise"];
+console.log("I am inviting ".concat(guests.length, " people to dinner."));
---
+++ Q19.ts
@@ -0,0 +1,6 @@
+let guests : string [] = ["Cristiano", "Kristen", "Dicaprio", "Tom Cruise"]
+
+console.log(`I am inviting ${guests.length} people to dinner.`);
+
+
+   
