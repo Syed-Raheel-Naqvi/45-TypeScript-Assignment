bd019327d87810a0b8f594c74f9215e025cf3350
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-09 02:31:34 +0500

    Add files via upload

---
+++ Q22.js
@@ -0,0 +1,4 @@
+"use strict";
+let Icons = ["Bill Gates", "Steve Jobs", "Elon Musk", "Mark Zukerberg", "Einstein"];
+console.log(Icons[5]); // Intentional error: Arrays are zero-indexed, so index 5 is out of bounds.
+console.log(Icons[4]); //// Correcting the error by accessing a valid index.tsct
---
+++ Q22.ts
@@ -0,0 +1,5 @@
+let Icons : string [] = ["Bill Gates","Steve Jobs", "Elon Musk", "Mark Zukerberg", "Einstein"]
+
+console.log(Icons[5]); // Intentional error: Arrays are zero-indexed, so index 5 is out of bounds.
+console.log(Icons[4]); //// Correcting the error by accessing a valid index.tsct
+
