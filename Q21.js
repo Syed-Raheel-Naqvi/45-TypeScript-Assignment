e9fcba95e1f9ca95d4a0cd8821665d5bab8f97ab
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-09 00:37:22 +0500

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,7 @@
+"use strict";
+let book = {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K.Rowling",
+    publishedYear: 1997,
+};
+console.log(`Book info, ${book.title} ${book.author}, ${book.publishedYear}`);
---
+++ Q21.ts
@@ -0,0 +1,8 @@
+let book : {title : string, author : string, publishedYear : number} = {
+    title : "Harry Potter and the Sorcerer's Stone",
+    author : "J.K.Rowling",
+    publishedYear : 1997,
+};
+
+console.log(`Book info, ${book.title} ${book.author}, ${book.publishedYear}`);
+
