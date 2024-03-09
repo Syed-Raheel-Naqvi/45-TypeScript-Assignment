d54a38b3e4d03a6f866347975159e7fa232de9dc
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-09 05:04:38 +0500

    Add files via upload

---
+++ Q23.js
@@ -0,0 +1,24 @@
+"use strict";
+let car = "subaru";
+console.log("is car == 'subaru'?  I predict True.");
+console.log(car == 'subaru'); // True
+console.log("car == 'mercedes'? I predict False.");
+console.log(car == 'mercedes'); //False
+console.log("car.length == 6 ? I predict True.");
+console.log(car.length == 6); //True
+console.log("car.length == 8 ? I predict False.");
+console.log(car.length == 8); //False
+console.log("car.length == 6 && car == 'subaru' I predict True.");
+console.log(car.length == 6 && car == 'subaru'); //True
+console.log("car.length == 8 && car == 'subaru' I predict True.");
+console.log(car.length == 8 && car == 'subaru'); //False
+let num1 = 25;
+let num2 = 45;
+console.log("Is num1 != num2 ? I predict True.");
+console.log(num1 != num2); //True
+console.log("Is num1 == num2 ? I predict False.");
+console.log(num1 == num2); //False
+console.log("Is num1 < num2? I predict True.");
+console.log(num1 < num2); //True
+console.log("Is num1 > num 2? I predict False.");
+console.log(num1 > num2); //False
