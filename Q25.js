e671490d9de95122e4bb0c568d4794015cfd971f
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-11 01:23:12 +0500

    Add files via upload

---
+++ Q25.js
@@ -0,0 +1,9 @@
+"use strict";
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earn five points!");
+}
+if (alien_color == "red") {
+    console.log("You just earn five points!");
+}
+//No output because the condition is false
---
+++ Q25.ts
@@ -0,0 +1,10 @@
+let alien_color : string = "green"
+
+if(alien_color == "green"){
+ console.log("You just earn five points!");
+}
+
+if(alien_color == "red"){
+    console.log("You just earn five points!");
+   }
+ //No output because the condition is false
