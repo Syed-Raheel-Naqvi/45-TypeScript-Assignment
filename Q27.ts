3674dc5dd919d143acea71a8c449ea9d1f13c19b
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-11 01:24:54 +0500

    Add files via upload

---
+++ Q27.ts
@@ -0,0 +1,29 @@
+let alien_color = "green"
+
+//This version runs "Green" alien
+if(alien_color == "green"){
+    console.log("You just earned 5 points");    
+}else if(alien_color == "yellow"){
+    console.log("You just earned 10 points");    
+}else if(alien_color == "red"){
+    console.log("You just earned 15 points");    
+}
+
+//This version runs "Yellow" alien
+alien_color = "yellow"
+if(alien_color == "green"){
+    console.log("You just earned 5 points");    
+}else if(alien_color == "yellow"){
+    console.log("You just earned 10 points");    
+}else if(alien_color == "red"){
+    console.log("You just earned 15 points");    
+}
+
+//This version runs "Red" alien
+if(alien_color == "red"){
+    console.log("You just earned 5 points");    
+}else if(alien_color == "yellow"){
+    console.log("You just earned 10 points");    
+}else if(alien_color == "red"){
+    console.log("You just earned 15 points");    
+}
