d8435633629d67210191226ad7775802945a999a
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-11 23:54:20 +0500

    Add files via upload

---
+++ Q29.js
@@ -0,0 +1,17 @@
+"use strict";
+let favorite_fruits = ["Mango", "Banana", "Pineapple"];
+if (favorite_fruits.includes("Mango")) {
+    console.log("You really like Mangoes!");
+}
+if (favorite_fruits.includes("Banana")) {
+    console.log("You like Bananas!");
+}
+if (favorite_fruits.includes("Orange")) {
+    console.log("You really like Oranges!");
+}
+if (favorite_fruits.includes("Apple")) {
+    console.log("You really like Apples!");
+}
+if (favorite_fruits.includes("Pineapple")) {
+    console.log("You really like Pineapples!");
+}
---
+++ Q29.ts
@@ -0,0 +1,17 @@
+let favorite_fruits : string [] = ["Mango", "Banana", "Pineapple"]
+
+if(favorite_fruits.includes("Mango")){
+    console.log("You really like Mangoes!");
+}
+if(favorite_fruits.includes("Banana")){
+    console.log("You like Bananas!");
+}
+if(favorite_fruits.includes("Orange")){
+    console.log("You really like Oranges!");
+}
+if(favorite_fruits.includes("Apple")){
+    console.log("You really like Apples!");
+}
+if(favorite_fruits.includes("Pineapple")){
+    console.log("You really like Pineapples!");
+}
