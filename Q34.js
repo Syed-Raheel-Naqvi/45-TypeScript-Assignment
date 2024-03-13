32747c3a2906441f93c0aae2abd8cb3a5b96993c
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-13 22:36:52 +0500

    Add files via upload

---
+++ Q34.js
@@ -0,0 +1,11 @@
+"use strict";
+// let favouritePizzas : string [] = ["Chicken Tikka", "Chicken Fajita", "Malai Boti"]
+// for(let i = 0; i < favouritePizzas.length; i++){
+// console.log(`I like ${favouritePizzas[i]} Pizza.`);
+// }
+// console.log("i Really Love Pizza.");
+let pizzas = ["Chicken Tikka", "Chicken Fajita", "pepperoni"];
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza} pizza`);
+});
+console.log("I really love pizza!");
---
+++ Q34.ts
@@ -0,0 +1,6 @@
+let pizzas : string [] = ["Chicken Tikka", "Chicken Fajita", "pepperoni"];
+
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza} pizza`);
+});
+console.log("I really love pizza!");
