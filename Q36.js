2230c620e350bf5b0bf21fca86be89bfd841d674
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-13 22:38:04 +0500

    Add files via upload

---
+++ Q36.js
@@ -0,0 +1,5 @@
+"use strict";
+function make_shirt(size, message) {
+    console.log(`I'm making a "${size}" T-shirt with the message "${message}"`);
+}
+make_shirt("medium", "I Love Typescript");
---
+++ Q36.ts
@@ -0,0 +1,5 @@
+function make_shirt(size : string, message : string){
+console.log(`I'm making a "${size}" T-shirt with the message "${message}"`);
+}
+make_shirt("medium", "I Love Typescript");
+
