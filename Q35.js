3be1ecfc984c9989e3203391143bfde5919a1011
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-13 22:37:30 +0500

    Add files via upload

---
+++ Q35.js
@@ -0,0 +1,6 @@
+"use strict";
+let animals = ["Lion", "cat", "Dog"];
+animals.forEach(animal => {
+    console.log(`${animal} would make a great pet`);
+});
+console.log("Any of these animals would make a great pet!");
---
+++ Q35.ts
@@ -0,0 +1,7 @@
+let animals : string [] = ["Lion", "cat", "Dog"]
+
+animals.forEach(animal => {
+    console.log(`${animal} would make a great pet`);
+});
+console.log("Any of these animals would make a great pet!");
+
