ddd79c6e278e3aeac721b6b8b251c9530d584ea9
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-14 22:11:58 +0500

    Add files via upload

---
+++ Q39.ts
@@ -0,0 +1,6 @@
+function city_country(city : string, country : string){
+     return (`${city} ${country}`);
+}
+ console.log(city_country("Karachi", "Pakistan"));
+console.log(city_country("Madrid", "Spain"));
+console.log(city_country("Venice","Italy"));
