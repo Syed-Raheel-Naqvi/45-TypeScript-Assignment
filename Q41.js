316187ab37facb4f1b3168eab4feef4e3e5f0414
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-16 01:47:02 +0500

    Add files via upload

---
+++ Q41.js
@@ -0,0 +1,8 @@
+"use strict";
+let magicians = ["David Coverfield", "Criss Angel", "Alice"];
+function show_magician(magicians) {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+show_magician(magicians);
---
+++ Q41.ts
@@ -0,0 +1,8 @@
+let magicians = ["David Coverfield", "Criss Angel", "Alice"]
+
+function show_magician(magicians : string[]){
+    magicians.forEach(magician =>{
+      console.log(magician);
+    })
+}
+show_magician(magicians)
