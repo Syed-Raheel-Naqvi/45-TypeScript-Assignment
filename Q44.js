39d5360a1b1cd9fef5021cc15c37f205807fb1d1
Syed Raheel Naqvi <161625913+Syed-Raheel-Naqvi@users.noreply.github.com>
2024-03-17 21:09:42 +0500

    Add files via upload

---
+++ Q44.js
@@ -0,0 +1,8 @@
+"use strict";
+function sandwich(...items) {
+    console.log("Making a Sandwich with:");
+    console.log(`-${items}`);
+}
+sandwich("Bacon", "egg", "cheese");
+sandwich("Chicken", "fillet", "roll");
+sandwich("Ham", "cheese");
---
+++ Q44.ts
@@ -0,0 +1,9 @@
+ function sandwich(...items : string[]){
+   console.log("Making a Sandwich with:");
+   console.log(`-${items}`);
+}
+
+sandwich("Bacon", "egg", "cheese")
+sandwich("Chicken", "fillet", "roll")
+sandwich("Ham", "cheese")
+
